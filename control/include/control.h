30ad8d79c6ecb142e1521d06e959139428c33af3
JunSpring <wjdwnsqja08@gmail.com>
2023-01-16 11:50:16 +0900

    pure_pursuit 추가 및 imu subscribe 추가

--- control/include/control.h
+++ control/include/control.h
@@ -8,47 +8,75 @@
 
 #include<geometry_msgs/PointStamped.h>
 
-#include<morai_msgs/CtrlCmd.h>
+#include<visualization_msgs/MarkerArray.h>
+
+#include<std_msgs/Float64.h>
 
 #include<tf/tf.h>
 #include<tf2_msgs/TFMessage.h>
+
+#include<sensor_msgs/Imu.h>
+
+// #include <behaviortree_cpp_v3/loggers/bt_zmq_publisher.h>
+
+// // DWA include files
+// #include <tf/transform_listener.h>
+// #include <costmap_2d/costmap_2d_ros.h>
+// #include <dwa_local_planner/dwa_planner_ros.h>
 
 class local_path{
 public:
     local_path();
     
-    int speed;
     int num;
     bool flag;
+    bool is_look_foward_point;
+    double L, VL;
     double vehicle_roll;
     double vehicle_pitch;
     double vehicle_yaw;
+    double max_lfd, min_lfd;
     double rate = 10;
+    double offset = 335.7699221348003;
+    double steer_offset = 19.4799995422;
+
+    double bx;
+    double by;
+    double bz;
+    double bw;
 
     nav_msgs::Path path;
     nav_msgs::Path tracking_path;
 
     geometry_msgs::PoseStamped pose;
 
-    morai_msgs::CtrlCmd cmd_vel;
+    std_msgs::Float64 speed;
+    std_msgs::Float64 position;
 
     tf2_msgs::TFMessage tfmsg;
 
     //Publish
-    ros::Publisher cmd_pub;
+    ros::Publisher speed_pub;
+    ros::Publisher position_pub;
     ros::Publisher path_pub;
+    ros::Publisher marker_lfd;
+    ros::Publisher local_goal_pub;
 
     // Subscriber
     ros::Subscriber path_sub;
     ros::Subscriber tf_sub;
+    ros::Subscriber imu_sub;
 
     // Callback
     void pathcallback(const nav_msgs::Path& msg);
     void tfcallback(const tf2_msgs::TFMessage& msg);
+    void imucallback(const sensor_msgs::Imu& msg);
     
     //Function
+    double pure_pursuit();
     double stanley();
     double velocity();
+    double nomalize_angle(double);
     void path_tracking();
     void process();
 };
